8ed0299c903cdf282178f0f73a7c065a45579095
GangaramD <Gangaram136@gmail.com>
2019-01-16 01:06:29 +0530

    Initails commit

---
+++ ip_capture.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "[nginx]
+master ansible_host=$1 ansible_ssh_user=root ansible_ssh_pass=$2" >> inventory
+
+echo "[all:vars]
+ansible_python_interpreter=/usr/bin/python" >> inventory
